fad302f1e3412c6c9455d85e99a5e4e1a9ddf36c
T <68776253+sttrex-13@users.noreply.github.com>
2021-11-07 20:31:09 +0700

    Add function version1

---
+++ Assignment07/productmy.js
@@ -0,0 +1,56 @@
+import { products } from "./ProductList.mjs";
+let num = 0
+const cart = [];
+let click = false;
+const dataCount = document.querySelectorAll(".has-badge")[0];
+let amount = 0;
+
+function addNewProduct(productName,price,img,id){
+
+    let product = document.createElement("div"); //create element div by assign to product
+    product.setAttribute("class","product"); //set attribute class product in varriable product
+
+    let image = document.createElement("img"); //create element img by assign to image
+    image.setAttribute("src",img); //set attribute src that is img in each product
+
+    let name = document.createElement("div"); //create element div by assign to name
+    name.setAttribute("id","name"); //set attribute id that is price
+    name.innerHTML = productName; //set value in tag div by productName
+
+    let pricePD = document.createElement("div"); //create element div by assign to pricePD
+    pricePD.setAttribute("id","price"); //set attribute id that is price
+    pricePD.innerHTML = price; //set value in tag div by price 
+
+    let button = document.createElement("button"); //create element button by assign to variable button
+    button.setAttribute("class","button"); //set attribute class is button
+    button.innerHTML = `<i class="fa fa-shopping-cart" aria-hidden="true"></i> ADD TO CART` ;//assign value <i...> in button by innerHTML
+
+    product.append(image,name,pricePD,button); //merge element from image,name,pricePD,button to be child the product 
+
+    button.addEventListener("click",() => {
+        alert(`${productName} has been added to your cart.`);
+        let item = { productId: id, productName: productName, price: price };
+        cart.push(item);
+        amount++;
+        dataCount.setAttribute("data-count",amount);
+        console.log(cart);
+    });
+    
+    return product;
+
+}
+
+products.forEach((products) =>{  
+    //loop addNewProduct in each product from ProductList.mjs 
+    let product = addNewProduct(
+            products.productName,
+            products.price,
+            products.img,
+            products.productId
+        )  
+        
+     let productsList = document.querySelector("#products") // get tag that has class name products
+     productsList.append(product);  //and then merge product from function to be child in tag div calss products
+}
+    
+);
